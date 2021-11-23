6d2a8ed6b1f601cacac67e81e32fc2a59c565a77
Nikolay Nikolaev <evrinoma@gmail.com>
2021-11-23 20:50:35 +0300

    init DI configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -22,6 +22,33 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder      = new TreeBuilder(EvrinomaContractBundle::CONTRACT_BUNDLE);
         $rootNode         = $treeBuilder->getRootNode();
+        $supportedDrivers = ['orm'];
+
+        $rootNode
+            ->addDefaultsIfNotSet()
+            ->children()
+            ->scalarNode('db_driver')
+            ->validate()
+            ->ifNotInArray($supportedDrivers)
+            ->thenInvalid('The driver %s is not supported. Please choose one of '.json_encode($supportedDrivers))
+            ->end()
+            ->cannotBeOverwritten()
+            ->defaultValue('orm')
+            ->end()
+            ->scalarNode('factory_side')->cannotBeEmpty()->defaultValue(EvrinomaContractExtension::FACTORY_SIDE)->end()
+            ->scalarNode('factory_contract')->cannotBeEmpty()->defaultValue(EvrinomaContractExtension::FACTORY_CONTRACT)->end()
+            ->scalarNode('entity_side')->cannotBeEmpty()->defaultValue(EvrinomaContractExtension::ENTITY_BASE_SIDE)->end()
+            ->scalarNode('entity_contract')->cannotBeEmpty()->defaultValue(EvrinomaContractExtension::ENTITY_BASE_CONTRACT)->end()
+            ->scalarNode('constraints_contract')->defaultTrue()->info('This option is used for enable/disable basic contract constraints')->end()
+            ->scalarNode('constraints_side')->defaultTrue()->info('This option is used for enable/disable basic side constraints')->end()
+            ->scalarNode('dto_contract')->cannotBeEmpty()->defaultValue(EvrinomaContractExtension::DTO_BASE_CONTRACT)->info('This option is used for dto class override')->end()
+            ->scalarNode('dto_side')->cannotBeEmpty()->defaultValue(EvrinomaContractExtension::DTO_BASE_SIDE)->info('This option is used for dto class override')->end()
+            ->arrayNode('decorates')->addDefaultsIfNotSet()->children()
+            ->scalarNode('command_contract')->defaultNull()->info('This option is used for command contract decoration')->end()
+            ->scalarNode('query_contract')->defaultNull()->info('This option is used for query contract decoration')->end()
+            ->scalarNode('command_side')->defaultNull()->info('This option is used for command side decoration')->end()
+            ->scalarNode('query_side')->defaultNull()->info('This option is used for query side decoration')->end()
+            ->end()->end()->end();
 
         return $treeBuilder;
     }
