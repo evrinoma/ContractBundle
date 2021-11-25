7548f8496ecf59c31c2aede81f3959a44d1e8f50
Nikolay Nikolaev <evrinoma@gmail.com>
2021-11-25 12:49:51 +0300

    fix alias

--- src/Repository/AliasInterface.php
+++ src/Repository/AliasInterface.php
@@ -5,7 +5,7 @@ namespace Evrinoma\ContractBundle\Repository;
 interface AliasInterface
 {
 //region SECTION: Fields
-    public const BUNCH = 'contract';
+    public const CONTRACT = 'contract';
     public const SIDE  = 'side';
     public const TYPE  = 'type';
     public const HIERARCHY = 'hierarchy';
