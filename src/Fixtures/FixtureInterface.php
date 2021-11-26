2e58c6241751686933ae0789af304fde48cd3279
Nikolay Nikolaev <evrinoma@gmail.com>
2021-11-26 16:57:35 +0300

    init fixtures

--- src/Fixtures/FixtureInterface.php
+++ src/Fixtures/FixtureInterface.php
@@ -5,6 +5,7 @@ namespace Evrinoma\ContractBundle\Fixtures;
 interface FixtureInterface
 {
 //region SECTION: Fields
-    public const TYPE_FIXTURES  = 'TYPE_FIXTURES';
+    public const TYPE_FIXTURES      = 'TYPE_FIXTURES';
+    public const HIERARCHY_FIXTURES = 'HIERARCHY_FIXTURES';
 //endregion Fields
 }
---
+++ src/Fixtures/HierarchyFixtures.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Evrinoma\ContractBundle\Fixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Evrinoma\ContractBundle\Entity\Define\BaseHierarchy;
+
+final class HierarchyFixtures extends Fixture implements FixtureGroupInterface, OrderedFixtureInterface
+{
+//region SECTION: Fields
+    private array $data = [
+        ['identity' => 'contract'],
+        ['identity' => 'add_agr'],
+        ['identity' => 'contract_other'],
+        ['identity' => 'add'],
+        ['identity' => 'add_other'],
+        ['identity' => 'contract_sys'],
+    ];
+//endregion Fields
+
+//region SECTION: Public
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->createTypes($manager);
+
+        $manager->flush();
+    }
+//endregion Public
+//endregion Public
+
+//region SECTION: Private
+    private function createTypes(ObjectManager $manager)
+    {
+        $short = (new \ReflectionClass(BaseHierarchy::class))->getShortName()."_";
+        $i     = 0;
+
+        foreach ($this->data as $record) {
+            $entity = new BaseHierarchy();
+            $entity->setIdentity($record['identity']);
+            $this->addReference($short.$i, $entity);
+            $manager->persist($entity);
+            $i++;
+        }
+
+        return $this;
+    }
+
+//endregion Private
+
+//region SECTION: Getters/Setters
+    public static function getGroups(): array
+    {
+        return [
+            FixtureInterface::HIERARCHY_FIXTURES
+        ];
+    }
+
+    public function getOrder()
+    {
+        return 0;
+    }
+}
