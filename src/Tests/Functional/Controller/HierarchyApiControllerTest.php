599913f3d7227d691ec62b28957f75bf247caa2b
Nikolay Nikolaev <evrinoma@gmail.com>
2021-12-10 15:12:22 +0300

    refactoring controllers - as basic functional test

--- src/Tests/Functional/Controller/HierarchyApiControllerTest.php
+++ src/Tests/Functional/Controller/HierarchyApiControllerTest.php
@@ -3,105 +3,28 @@
 namespace Evrinoma\ContractBundle\Tests\Functional\Controller;
 
 
-use Evrinoma\ContractBundle\Dto\HierarchyApiDto;
 use Evrinoma\ContractBundle\Fixtures\FixtureInterface;
-use Evrinoma\TestUtilsBundle\Browser\ApiBrowserTestInterface;
-use Evrinoma\TestUtilsBundle\Browser\ApiBrowserTestTrait;
-use Evrinoma\TestUtilsBundle\Controller\ApiControllerTestInterface;
-use Evrinoma\TestUtilsBundle\Helper\ApiMethodTestInterface;
-use Evrinoma\TestUtilsBundle\Helper\ApiMethodTestTrait;
-use Evrinoma\TestUtilsBundle\Helper\ResponseStatusTestTrait;
-use Evrinoma\TestUtilsBundle\Web\AbstractWebCaseTest;
+use Evrinoma\TestUtilsBundle\Action\ActionTestInterface;
+use Evrinoma\TestUtilsBundle\Functional\AbstractFunctionalTest;
+use Psr\Container\ContainerInterface;
 
 /**
  * @group functional
  */
-class HierarchyApiControllerTest extends AbstractWebCaseTest implements ApiControllerTestInterface, ApiBrowserTestInterface, ApiMethodTestInterface
+final class HierarchyApiControllerTest extends AbstractFunctionalTest
 {
 //region SECTION: Fields
-    public const API_GET      = 'evrinoma/api/contract/hierarchy';
-    public const API_CRITERIA = 'evrinoma/api/contract/hierarchy/criteria';
-    public const API_DELETE   = 'evrinoma/api/contract/hierarchy/delete';
-    public const API_PUT      = 'evrinoma/api/contract/hierarchy/save';
-    public const API_POST     = 'evrinoma/api/contract/hierarchy/create';
+    protected string $actionServiceName = 'evrinoma.contract.type.test.functional.action.type';
 //endregion Fields
 
-    use ApiBrowserTestTrait, ApiMethodTestTrait, ResponseStatusTestTrait;
-
 //region SECTION: Protected
+    protected function getActionService(ContainerInterface $container): ActionTestInterface
+    {
+        return $container->get($this->actionServiceName);
+    }
 //endregion Protected
 
-//region SECTION: Public
-    public static function defaultData(): array
-    {
-        return [
-            "class"    => static::getDtoClass(),
-            "identity" => 'contract_own',
-        ];
-    }
-
-    public function testPost(): void
-    {
-        $this->assertEquals(true, true, 'test');
-    }
-
-    public function testCriteria(): void
-    {
-        $this->assertEquals(true, true, 'test');
-    }
-
-    public function testCriteriaNotFound(): void
-    {
-        $this->assertEquals(true, true, 'test');
-    }
-
-    public function testDelete(): void
-    {
-    }
-
-    public function testGet(): void
-    {
-    }
-
-    public function testPut(): void
-    {
-    }
-
-    public function testPutNotFound(): void
-    {
-    }
-
-    public function testPutUnprocessable(): void
-    {
-    }
-
-    public function testDeleteNotFound(): void
-    {
-    }
-
-    public function testDeleteUnprocessable(): void
-    {
-    }
-
-    public function testGetNotFound(): void
-    {
-    }
-
-    public function testPostDuplicate(): void
-    {
-    }
-
-    public function testPostUnprocessable(): void
-    {
-    }
-//endregion Public
-
 //region SECTION: Getters/Setters
-    public static function getDtoClass(): string
-    {
-        return HierarchyApiDto::class;
-    }
-
     public static function getFixtures(): array
     {
         return [FixtureInterface::HIERARCHY_FIXTURES];
--- src/Tests/Functional/Controller/TypeApiControllerTest.php
+++ src/Tests/Functional/Controller/TypeApiControllerTest.php
@@ -3,147 +3,28 @@
 namespace Evrinoma\ContractBundle\Tests\Functional\Controller;
 
 
-use Evrinoma\ContractBundle\Dto\TypeApiDto;
 use Evrinoma\ContractBundle\Fixtures\FixtureInterface;
-use Evrinoma\TestUtilsBundle\Browser\ApiBrowserTestInterface;
-use Evrinoma\TestUtilsBundle\Browser\ApiBrowserTestTrait;
-use Evrinoma\TestUtilsBundle\Controller\ApiControllerTestInterface;
-use Evrinoma\TestUtilsBundle\Helper\ApiMethodTestInterface;
-use Evrinoma\TestUtilsBundle\Helper\ApiMethodTestTrait;
-use Evrinoma\TestUtilsBundle\Helper\ResponseStatusTestTrait;
-use Evrinoma\TestUtilsBundle\Web\AbstractWebCaseTest;
+use Evrinoma\TestUtilsBundle\Action\ActionTestInterface;
+use Evrinoma\TestUtilsBundle\Functional\AbstractFunctionalTest;
+use Psr\Container\ContainerInterface;
 
 /**
  * @group functional
  */
-class TypeApiControllerTest extends AbstractWebCaseTest implements ApiControllerTestInterface, ApiBrowserTestInterface, ApiMethodTestInterface
+final class TypeApiControllerTest extends AbstractFunctionalTest
 {
 //region SECTION: Fields
-    public const API_GET      = 'evrinoma/api/contract/type';
-    public const API_CRITERIA = 'evrinoma/api/contract/type/criteria';
-    public const API_DELETE   = 'evrinoma/api/contract/type/delete';
-    public const API_PUT      = 'evrinoma/api/contract/type/save';
-    public const API_POST     = 'evrinoma/api/contract/type/create';
+    protected string $actionServiceName = 'evrinoma.contract.type.test.functional.action.type';
 //endregion Fields
 
-    use ApiBrowserTestTrait, ApiMethodTestTrait, ResponseStatusTestTrait;
-
 //region SECTION: Protected
+    protected function getActionService(ContainerInterface $container): ActionTestInterface
+    {
+        return $container->get($this->actionServiceName);
+    }
 //endregion Protected
 
-//region SECTION: Public
-    public static function defaultData(): array
-    {
-        return [
-            "class"    => static::getDtoClass(),
-            "identity" => 'main_income_own',
-        ];
-    }
-
-    public function testPostUnprocessable(): void
-    {
-        $this->postWrong();
-        $this->testResponseStatusUnprocessable();
-    }
-
-
-    public function testPost(): void
-    {
-        $this->createType();
-        $this->testResponseStatusCreated();
-    }
-
-    public function testPostDuplicate(): void
-    {
-        $this->createType();
-        $this->testResponseStatusCreated();
-
-        $this->createType();
-        $this->testResponseStatusConflict();
-    }
-
-    public function testPutUnprocessable(): void
-    {
-        $query = static::getDefault(['id' => '']);
-
-        $this->put($query);
-        $this->testResponseStatusUnprocessable();
-
-        $this->createType();
-
-        $query = static::getDefault(['identity' => '']);
-
-        $this->put($query);
-        $this->testResponseStatusUnprocessable();
-    }
-
-    public function testCriteria(): void
-    {
-        $this->assertEquals(true, true, 'test');
-    }
-
-    public function testCriteriaNotFound(): void
-    {
-        $this->assertEquals(true, true, 'test');
-    }
-
-    public function testDelete(): void
-    {
-    }
-
-    public function testGet(): void
-    {
-    }
-
-    public function testPut(): void
-    {
-    }
-
-    public function testPutNotFound(): void
-    {
-    }
-
-    public function testDeleteNotFound(): void
-    {
-    }
-
-    public function testDeleteUnprocessable(): void
-    {
-
-    }
-
-    public function testGetNotFound(): void
-    {
-    }
-
-    private function createTypeDuplicateIdentity(): array
-    {
-        $query = static::getDefault(['identity' => 'main_income']);
-
-        return $this->post($query);
-    }
-
-    private function createType(): array
-    {
-        $query = static::getDefault();
-
-        return $this->post($query);
-    }
-
-    private function createConstraintBlankId(): array
-    {
-        $query = static::getDefault(['id' => '']);
-
-        return $this->post($query);
-    }
-//endregion Public
-
 //region SECTION: Getters/Setters
-    public static function getDtoClass(): string
-    {
-        return TypeApiDto::class;
-    }
-
     public static function getFixtures(): array
     {
         return [FixtureInterface::TYPE_FIXTURES];
