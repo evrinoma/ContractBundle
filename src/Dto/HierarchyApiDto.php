99b30c1339594dbe6edac8ada59df4894710ee56
Nikolay Nikolaev <evrinoma@gmail.com>
2021-12-16 00:13:55 +0300

    fix Dto - remove wrong Interface

--- src/Dto/HierarchyApiDto.php
+++ src/Dto/HierarchyApiDto.php
@@ -7,10 +7,9 @@ use Evrinoma\DtoBundle\Dto\AbstractDto;
 use Evrinoma\DtoBundle\Dto\DtoInterface;
 use Evrinoma\DtoCommon\ValueObject\IdentityTrait;
 use Evrinoma\DtoCommon\ValueObject\IdTrait;
-use Evrinoma\HeaderBundle\Dto\HeaderApiDtoInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-class HierarchyApiDto extends  AbstractDto implements HeaderApiDtoInterface
+class HierarchyApiDto extends  AbstractDto implements HierarchyApiDtoInterface
 {
     use IdTrait, IdentityTrait;
 
