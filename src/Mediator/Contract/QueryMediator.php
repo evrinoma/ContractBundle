ee5312d7d5d0f12628bac1dbae91f3efa8aae060
Nikolay Nikolaev <evrinoma@gmail.com>
2022-01-22 16:01:52 +0300

    added logic query mediator

--- src/Mediator/Contract/QueryMediator.php
+++ src/Mediator/Contract/QueryMediator.php
@@ -3,6 +3,7 @@
 namespace Evrinoma\ContractBundle\Mediator\Contract;
 
 use Doctrine\ORM\QueryBuilder;
+use Evrinoma\ContractBundle\Dto\ContractApiDtoInterface;
 use Evrinoma\ContractBundle\Repository\AliasInterface;
 use Evrinoma\DtoBundle\Dto\DtoInterface;
 use Evrinoma\UtilsBundle\Mediator\AbstractQueryMediator;
@@ -18,6 +19,56 @@ class QueryMediator extends AbstractQueryMediator implements QueryMediatorInterf
     {
         $alias = $this->alias();
 
+        /** @var $dto ContractApiDtoInterface */
+        if ($dto->hasTypeApiDto()) {
+            $aliasType = AliasInterface::TYPE;
+            $builder
+                ->leftJoin($alias.'.type', $aliasType)
+                ->addSelect($aliasType);
+
+            if ($dto->getTypeApiDto()->hasId()) {
+                $builder->andWhere($aliasType.'.id = :idType')
+                    ->setParameter('idType', $dto->getTypeApiDto()->getId());
+            }
+            if ($dto->getTypeApiDto()->hasIdentity()) {
+                $builder->andWhere($aliasType.'.identity like :identityType')
+                    ->setParameter('identityType', '%'.$dto->getTypeApiDto()->getIdentity().'%');
+            }
+        }
+
+        if ($dto->hasHierarchyApiDto()) {
+            $aliasHierarchy = AliasInterface::HIERARCHY;
+            $builder
+                ->leftJoin($alias.'.hierarchy', $aliasHierarchy)
+                ->addSelect($aliasHierarchy);
+
+            if ($dto->getHierarchyApiDto()->hasId()) {
+                $builder->andWhere($aliasHierarchy.'.id = :idHierarchy')
+                    ->setParameter('idHierarchy', $dto->getHierarchyApiDto()->getId());
+            }
+            if ($dto->getHierarchyApiDto()->hasIdentity()) {
+                $builder->andWhere($aliasHierarchy.'.identity like :identityHierarchy')
+                    ->setParameter('identityHierarchy', '%'.$dto->getHierarchyApiDto()->getIdentity().'%');
+            }
+        }
+
+        if ($dto->hasDescription()) {
+            $builder
+                ->andWhere($alias.'.description like :description')
+                ->setParameter('description', '%'.$dto->getDescription().'%');
+        }
+
+        if ($dto->hasName()) {
+            $builder
+                ->andWhere($alias.'.name like :name')
+                ->setParameter('name', '%'.$dto->getName().'%');
+        }
+
+        if ($dto->hasActive()) {
+            $builder
+                ->andWhere($alias.'.active = :active')
+                ->setParameter('active', $dto->getActive());
+        }
     }
 //endregion Public
 }
