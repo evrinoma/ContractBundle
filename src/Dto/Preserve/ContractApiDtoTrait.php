3f3e2389fc5a8bea0796d8afcdc32ea3f4be0c5d
Nikolay Nikolaev <evrinoma@gmail.com>
2022-01-28 22:28:46 +0300

    add preserve logic

---
+++ src/Dto/Preserve/ContractApiDtoTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Evrinoma\ContractBundle\Dto\Preserve;
+
+use Evrinoma\ContractBundle\Dto\ContractApiDto;
+
+trait ContractApiDtoTrait
+{
+    /**
+     * @param string $active
+     */
+    public function setActive(string $active): void
+    {
+        parent::setActive($active);
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        parent::setId($id);
+    }
+
+    /**
+     * @param string $number
+     */
+    public function setNumber(string $number): void
+    {
+        parent::setNumber($number);
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        parent::setDescription($description);
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        parent::setName($name);
+    }
+
+}
